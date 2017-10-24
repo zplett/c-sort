9f483f8188033223dbe14dfc6cc213156bfb6c7a
Ezra Goss <egoss@clyde>
2017-10-23 23:27:20 -0400

    Added qsort: not using comparator?

--- sort.c
+++ sort.c
@@ -4,35 +4,49 @@ Ezra Goss Zac Plett
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #define MAX_LINES 1024*1024
 #define LINE_LENGTH 1024
 
 // Static function declarations
-static void populate_array(char *lines[]);
+static int populate_array(char *lines[]);
+static int ascii_compar( const void*, const void* );
+static int alpha_compar( const void*, const void* );
+static int numeric_compar( const char*, const char* );
 
 int main( int argc, char *argv[] ) {
+  // Allocate for outer array
   char **lines = malloc( MAX_LINES * sizeof(char*) );
+  // Allocate for inner array
   for( int i = 0; i < MAX_LINES; ++i ) {
-    lines[i] = malloc( LINE_LENGTH * sizeof(char) + 1 );
+    lines[i] = malloc( LINE_LENGTH * sizeof(char) );
   }
-  populate_array(lines);
+  // Populate the arrays
+  int line_count = populate_array(lines);
+  // Sort the lines
+  // TODO: not calling comparator
+  qsort( (void*)lines, line_count, sizeof(lines[0]), ascii_compar );
+  
   for( int i = 0; i < MAX_LINES; ++i) {
-    for( char *ptr = lines[i] ; *ptr == '\0' ; ++ptr ){
+    for( char* ptr = lines[i] ; *ptr != '\0'; ++ptr ){
       printf("%c", *ptr);
     }
-    if( *lines[i + 1] == '\0' ) break;
+    if( *lines[i + 1] == '\0' )
+      break;
   }
-    
+   
+  // Free inner arrays
   for(int i = 0; i < MAX_LINES; ++i) {
     free(lines[i]);
   }
+  // Free outer array
   free(lines);
 }
   
 /** Populate the array with line input
 
  */
-void populate_array( char *lines[] ){
+int populate_array( char *lines[] ){
   // Line number
   unsigned int i = 0;
   // Pointer to the first character of line i
@@ -48,4 +62,37 @@ void populate_array( char *lines[] ){
       // Point to the next character slot
       ++line;
   }
+}
+
+/** Compare the ascii values of two characters
+ *
+ */
+int ascii_compar( const void *c1, const void *c2 ) {
+  printf("Ascci compare\n");
+  // If the ascii value is greater
+  if( *(char*)c1 > *(char*)c2 ) return 1;
+  // If the ascii value is less than
+  else if( *(char*)c1 < *(char*)c2 ) return -1;
+
+  // If the ascii values are equal
+  return 0;
+}
+
+/** Compare the alphabetical value of two characters (case insensitive)
+ *
+ */
+int alpha_compar( const void *c1, const void *c2 ) {
+  // Dummy variables
+  char l1, l2;
+  // Make the comparison case insensitive with new variables
+  if( isalpha(*(char*)c1) && (*(char*)c2) ) {
+    l1 = tolower( *(char*)c1 );
+    l2 = tolower( *(char*)c2 );
+  } else {
+    l1 = *(char*)c1;
+    l2 = *(char*)c2;
+  }
+  
+  // Run ascii_compar on altered characters
+  return ascii_compar( &l1, &l2 );
 }
