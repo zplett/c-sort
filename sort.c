34a2d1deb734d6f0d5f3b6b9be3725b0780b8c39
Ezra Goss <egoss@clyde>
2017-10-24 00:02:47 -0400

    sorting works now

--- sort.c
+++ sort.c
@@ -9,7 +9,7 @@ Ezra Goss Zac Plett
 #define LINE_LENGTH 1024
 
 // Static function declarations
-static int populate_array(char *lines[]);
+static int populate_array( char *lines[] );
 static int ascii_compar( const void*, const void* );
 static int alpha_compar( const void*, const void* );
 static int numeric_compar( const char*, const char* );
@@ -29,18 +29,18 @@ int main( int argc, char *argv[] ) {
   
   for( int i = 0; i < MAX_LINES; ++i) {
     for( char* ptr = lines[i] ; *ptr != '\0'; ++ptr ){
-      printf("%c", *ptr);
+      printf( "%c", *ptr );
     }
     if( *lines[i + 1] == '\0' )
       break;
   }
    
   // Free inner arrays
-  for(int i = 0; i < MAX_LINES; ++i) {
-    free(lines[i]);
+  for( int i = 0; i < MAX_LINES; ++i ) {
+    free( lines[i] );
   }
   // Free outer array
-  free(lines);
+  free( lines );
 }
   
 /** Populate the array with line input
@@ -62,18 +62,17 @@ int populate_array( char *lines[] ){
       // Point to the next character slot
       ++line;
   }
-  return i + 1;
+  return i;
 }
 
 /** Compare the ascii values of two characters
  *
  */
 int ascii_compar( const void *c1, const void *c2 ) {
-  printf("Ascci compare\n");
   // If the ascii value is greater
-  if( *(char*)c1 > *(char*)c2 ) return 1;
+  if( **(char**)c1 > **(char**)c2 ) return 1;
   // If the ascii value is less than
-  else if( *(char*)c1 < *(char*)c2 ) return -1;
+  else if( **(char**)c1 < **(char**)c2 ) return -1;
 
   // If the ascii values are equal
   return 0;
@@ -84,16 +83,16 @@ int ascii_compar( const void *c1, const void *c2 ) {
  */
 int alpha_compar( const void *c1, const void *c2 ) {
   // Dummy variables
-  char l1, l2;
+  char l1, l2, *p1 = &l1, *p2 = &l2;
   // Make the comparison case insensitive with new variables
-  if( isalpha(*(char*)c1) && (*(char*)c2) ) {
-    l1 = tolower( *(char*)c1 );
-    l2 = tolower( *(char*)c2 );
+  if( isalpha(**(char**)c1) && (**(char**)c2) ) {
+    l1 = tolower( **(char**)c1 );
+    l2 = tolower( **(char**)c2 );
   } else {
-    l1 = *(char*)c1;
-    l2 = *(char*)c2;
+    l1 = **(char**)c1;
+    l2 = **(char**)c2;
   }
   
   // Run ascii_compar on altered characters
-  return ascii_compar( &l1, &l2 );
+  return ascii_compar( &p1, &p2 );
 }
