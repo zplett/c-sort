c42eae13eb31bcd22ae74a1f59d3818a1d1dbca5
Ezra Goss <ezragoss@me.com>
2017-10-23 21:31:41 -0400

    Added malloc, bad build

--- sort.c
+++ sort.c
@@ -5,13 +5,16 @@ Ezra Goss Zac Plett
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_LINES 1024*1024
-
+#define LINE_LENGTH 1024
 
 // Static function declarations
 static void populate_array(char *lines[]);
 
-int main(int argc, char *argv[]) {
-  char **lines = malloc( MAX_LINES );
+int main( int argc, char *argv[] ) {
+  char **lines = (char **)malloc( MAX_LINES );
+  for( int i = 0; i < MAX_LINES; ++i ){
+    lines[i] = (char *)malloc( LINE_LENGTH );
+  }
   populate_array(lines);
   /*  for(char* ptr = lines[0]; *ptr != '\0'; ++ptr){
     printf("%c", *ptr);
@@ -22,10 +25,10 @@ int main(int argc, char *argv[]) {
 /** Populate the array with line input
 
  */
-void populate_array(char *lines[]){
+void populate_array( char *lines[] ){
   // Line number
   unsigned int i = 0;
-  // Pointer to the first character of line i 
+  // Pointer to the first character of line i
   char *line = lines[i];
   // Input loop
   for( int c = getchar(); c != EOF; c = getchar()) {
