24400fc475851008f0b64eea3b4d348a333611b1
Zac Plett <zplett@clyde>
2017-10-24 00:18:50 -0400

    added flags, fixed seg faults

--- sort.c
+++ sort.c
@@ -2,6 +2,7 @@
 Ezra Goss Zac Plett
  */
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -15,6 +16,18 @@ static int alpha_compar( const void*, const void* );
 static int numeric_compar( const char*, const char* );
 
 int main( int argc, char *argv[] ) {
+
+  int fold_flag = 0, num_flag = 0;
+  if ( argc > 1) {
+    
+    if ( strcmp(argv[1], "-f") == 0 )
+      fold_flag = 1;
+
+    else if ( strcmp(argv[1], "-n") == 0 )
+      num_flag = 1;
+
+  }
+  
   // Allocate for outer array
   char **lines = malloc( MAX_LINES * sizeof(char*) );
   // Allocate for inner array
@@ -25,12 +38,14 @@ int main( int argc, char *argv[] ) {
   int line_count = populate_array(lines);
   // Sort the lines
   // TODO: not calling comparator
-  qsort( (void*)lines, line_count, sizeof(lines[0]), ascii_compar );
-  
+  if ( fold_flag == 0 && num_flag == 0 )
+    qsort( (void*)lines, line_count, sizeof(lines[0]), ascii_compar );
+  else if ( fold_flag == 1 )
+    qsort( (void*)lines, line_count, sizeof(lines[0]), alpha_compar );
   for( int i = 0; i < MAX_LINES; ++i) {
     for( char* ptr = lines[i] ; *ptr != '\0'; ++ptr ){
       printf( "%c", *ptr );
-    }
+    } printf( "\n" );
     if( *lines[i + 1] == '\0' )
       break;
   }
@@ -53,16 +68,16 @@ int populate_array( char *lines[] ){
   char *line = lines[i];
   // Input loop
   for( int c = getchar(); c != EOF; c = getchar()) {
-    // Initialize character no matter what
-    *line = c;
     // If new line, point to the next line in the array
     if( c == '\n' )
       line = lines[++i];
-    else 
+    else {
+      *line = c;
       // Point to the next character slot
       ++line;
+    }
   }
-  return i;
+  return i + 1;
 }
 
 /** Compare the ascii values of two characters
